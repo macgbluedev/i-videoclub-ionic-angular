5755690400315343cc4433b3279c58a50cf86e1d
Moises Croes <mcg-360@hotmail.com>
2022-04-22 00:45:58 +0200

    Add items in Curriculum page, fix thenme in Author page

--- src/app/curriculum/curriculum.page.ts
+++ src/app/curriculum/curriculum.page.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurriculumPage implements OnInit {
 
-  constructor() { }
+  works: {
+    date: string,
+    description: string
+  }[];
+
+  constructor() {
+    this.works = [{date: '2021-2022',description: 'Software Developer Analyst - NTT Data (Everis)'},
+    {date: '2019-2021',description: 'Data Analyst - NTT Data (Everis)'},
+    {date: '2016-2018',description: 'Spare warehouse supervisor - Laboratorio Behrens'} ]
+   }
 
   ngOnInit() {
   }
